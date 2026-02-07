5c274e4b07ebf6d37db90c35d41ddf30e3b1d3dc
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-07 16:47:22 -0600

    common shell script for deploying roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ app_name="mongod"
 check_root_user
 
 #Copy mongod repo file to install mongodb
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
 status_check $? "Copying MongoDB repo file"
 
 if dnf list installed mongodb-org -y &>>$LOG_FILE; then
