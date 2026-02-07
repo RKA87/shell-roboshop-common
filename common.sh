b0ab5dec0d5df68ae2da430794cbb92fadfa934f
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-07 16:24:01 -0600

    common shell script for deploying roboshop

---
+++ common.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Color Code
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NO="\e[0m"
+
+LOG_FOLDER="/var/log/shell-roboshop-common"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+START_TIME=$(date "%s")
+
+mkdir -p $LOG_FOLDER #because we are passing LOG_FIL in starting itself
+
+echo -e "Script Started at : $(date "+%Y-%m-%d %H:%M:%S")" | tee -a $LOG_FILE
+
+# Check its a Root user or not
+check_root_user() {
+    USER_ID=$(id -u)
+    if [ $USER_ID -ne 0 ]; then
+    echo -e "${RED}You should be a root user to execute this script${NO}"
+    exit 1
+    else
+    echo -e "${GREEN}You are a root user, you can continue${NO}"
+    fi
+}
+
+# Validation Status Check
+status_check() {
+    if [ $1 -ne 0 ]; then
+    echo -e "($(date "+%Y-%m-%d %H:%M:%S")) $2.... ${RED} Failed${NO}"
+    exit 1
+    else
+    echo -e "($(date "+%Y-%m-%d %H:%M:%S")) $2.... ${GREEN} Success${NO}"
+    fi
+}
+
+# Systemctl Services Status
+systemctl_enable() {
+    systemctl enable "$app_name" &>>"$LOG_FILE"
+    status_check $? "Enabling $app_name service"
+}
+
+systemctl_start() {
+    systemctl start "$app_name" &>>"$LOG_FILE"
+    status_check $? "Starting $app_name service"
+}
+
+systemctl_restart() {
+    systemctl restart "$app_name" &>>"$LOG_FILE"
+    status_check $? "Restarting $app_name service"
+}
---
+++ mongodb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source ./common.sh
+app_name="mongod"
+
+SCRIPT_DIR=$(pwd)
+
+check_root_user
+
+
+#Copy mongod repo file to install mongodb
+cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo
+status_check $? "Copying MongoDB repo file"
+
+if dnf list installed mongodb-org -y &>>$LOG_FILE; then
+    echo -e "${YELLOW}MongoDB is already installed, skipping installation${NO}"
+else
+    #Install MongoDB
+    dnf install mongodb-org -y &>>$LOG_FILE
+    status_check $? "Installing MongoDB"
+fi
+
+
+systemctl_enable $app_name
+systemctl_start $app_name
+
+# # Update the bind_ip 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+status_check $? "Updating bind_ip to allow remote connections"
+
+systemctl_restart $app_name
