f1f2250327c329b75110c8e1466122e6c3cd1b48
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-10 12:06:24 -0600

    common shell script for deploying roboshop

--- payment.sh
+++ payment.sh
@@ -7,12 +7,9 @@ check_root_user
 useradd_creation
 
 #install python3 gcc and python3-devel
-if dnf list installed python3 &>>$LOG_FILE; then
-    echo -e "${YELLOW} python3 is already installed, skipping installation${NO}"
-else
-    dnf install python3 gcc python3-devel -y &>>$LOG_FILE
-    status_check $? "Installing python3"
-fi
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+status_check $? "Installing python3"
+
 
 python3_app_setup
 
