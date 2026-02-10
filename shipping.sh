d24f84474283afa83e7d26783f8566e7e9e6bf1d
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-10 13:27:22 -0600

    common shell script for deploying roboshop

--- shipping.sh
+++ shipping.sh
@@ -14,3 +14,17 @@ systemd_reload
 systemctl_enable
 systemctl_start
 systemctl_restart
+
+dnf install mysql -y &>>$LOG_FILE
+status_check $? "Installing MySQL client"
+
+mysql -h mysql.rkak87.online -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+status_check $? "Loading schema to MySQL"
+
+mysql -h mysql.rkak87.online -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+status_check $? "Loading app-user schema to MySQL"
+
+mysql -h mysql.rkak87.online -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+status_check $? "Loading master data to MySQL"
+
+systemctl_restart
